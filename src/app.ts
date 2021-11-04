5cf4b27d951e525d3924b8ccb1e8b8b409f9a16c
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-11-03 19:11:41 -0700

    arrays

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
-function sumAll(message, ...arr) {
-    console.log(message);
-    return arr.reduce((prev, next) => prev + next);
-}
+const toppings = ['bacon', 'chilli'];
 
-const sum = sumAll('Hello', 1,2,3,4,5,6,7,8,9,10);
+const newToppings = ['pepperoni'];
 
+const allToppings = [...newToppings, ...toppings];
 
-console.log(sum);
+console.log(allToppings);
