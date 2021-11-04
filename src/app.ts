cb3d06109fbd26474e1c2cfc6e1e518fdbbf299f
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-11-03 20:00:35 -0700

    string types

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
-const pizzaCost: number = 10;
-const pizzaToppings: number = 2;
+const coupon: string = 'pizza25';
 
-function calculatePrince(cost: number, toppings: number): number {
-    return cost + 1.5 * toppings;
+function normalizeCoupon(code: string): string {
+    return code.toUpperCase();
 }
 
-const cost: number = calculatePrince(pizzaCost, pizzaToppings);
+const couponMessage: string = `Final coupon is ${normalizeCoupon(coupon)}`;
 
-console.log(`Pizza costs: ${cost}`);
+console.log(couponMessage);
