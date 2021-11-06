f6f30f586cef3bb09bd46f3ceab33dbc70b3606d
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-11-06 12:26:45 -0700

    default

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,7 @@
 let sumOrder: (price: number, quantity?: number) => number;
 
-sumOrder = (x, y) => {
-    if (y) {
-        return x * y;
-    }
-    return x;
-}
+sumOrder = (x, y = 1) =>  x * y;
 
-const sum = sumOrder(25);
+const sum = sumOrder(25, 5);
 
 console.log(`Total sum: ${sum}`);
