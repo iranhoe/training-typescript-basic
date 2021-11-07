b231dde7e70d188e67637341baabbf7fe8c95405
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-11-07 12:37:06 -0700

    interfaces implements

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
-abstract class Sizes {
+interface SizesInterface {
+    availableSizes: string[];
+}
+
+abstract class Sizes implements SizesInterface {
     constructor(protected sizes: string[]) {}
 
     set availableSizes(sizes: string[]) {
@@ -10,7 +14,14 @@ abstract class Sizes {
     }
 }
 
-class Pizza extends Sizes {
+interface PizzaInterface extends SizesInterface {
+    readonly name: string;
+    toppings: string[];
+    updateSizes(sizes: string[]): void;
+    addTopping(topping: string): void;
+}
+
+class Pizza extends Sizes implements PizzaInterface {
     toppings: string[] = [];
 
     constructor(readonly name: string, public sizes: string[]) {
