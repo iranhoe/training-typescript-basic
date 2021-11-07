71880253d4e606ab25d8c5963960815e9e73de88
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-11-07 14:45:15 -0700

    call apply bind

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,19 @@
-function myFunction() {
-    console.log('Function:::', this);
-}
-myFunction();
-
-// Object literal
 const myObj = {
     myMethod() {
         console.log('Object:::', this);
     },
 };
-myObj.myMethod();
 
-// Classes
-class MyClass {
-    myMethod() {
-        console.log('Class:::', this);
-    }
+//myObj.myMethod();
+
+// Function
+function myFunction(...text: string[]) {
+    console.log('Function:::', this, text);
 }
 
-const myInstance = new MyClass();
-myInstance.myMethod();
+const bindFunction = myFunction.bind(myObj);
+bindFunction('ABC', 'DEF');
+bindFunction('123', '456');
+bindFunction('ABC', 'DEF');
+myFunction.call(myObj, 'ABC', 'DEF');
+myFunction.apply(myObj, ['ABC', 'DEF']);
