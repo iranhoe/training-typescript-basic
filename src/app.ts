baeb6f0ce233dd905b3a5d40faca22c65bf77a85
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-11-07 16:08:58 -0700

    keyof lookup

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,15 @@ const person = {
 
 type Person = typeof person;
 type PersonKeys = keyof Person;
-
 type PersonTypes = Person[PersonKeys];
+
+function getProperty<T, K extends keyof T>(obj: T, key: K){
+    return obj[key];
+}
+
+const personName = getProperty(person, 'age');
 
 const anotherPerson: Person = {
     name: 'John',
     age: 30
 };
-
-// JavaScript
-// typeof person // 'object'
