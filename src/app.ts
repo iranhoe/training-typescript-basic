ca7037da909df4d9a666297a4c92393b198addf5
Iran Corrales Becerra <iranhoe@live.com.mx>
2021-11-07 13:23:33 -0700

    this keyword

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
-function reverse(str: string): string;
-function reverse<T>(arr: T[]): T[];
-function reverse<T>(stringOrArray: string | T[]): string | T[] {
-    if (typeof stringOrArray === 'string') {
-        return stringOrArray
-            .split('')
-            .reverse()
-            .join('');
-    }
+function myFunction() {
+    console.log('Function:::', this);
+}
+myFunction();
 
-    return stringOrArray.slice().reverse();
+// Object literal
+const myObj = {
+    myMethod() {
+        console.log('Object:::', this);
+    },
+};
+myObj.myMethod();
+
+// Classes
+class MyClass {
+    myMethod() {
+        console.log('Class:::', this);
+    }
 }
 
-reverse('Pepperoni');
-reverse(['bacon', 'pepperoni', 'chili', 'mushrooms']);
+const myInstance = new MyClass();
+myInstance.myMethod();
